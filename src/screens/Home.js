a9b83d6d82324708fc6e747585c03b6ab9f0ebef
Júlia Almeida <51387195+juliaalmeida13@users.noreply.github.com>
2023-03-14 01:04:13 -0300

    Update home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Linking, Image } from "react-native";
+import {StyleSheet, View, Linking, Image} from "react-native";
 
 import {
   Layout,
@@ -12,8 +12,8 @@ import {
   themeColor,
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
-import minhaImagem from "../../assets/Chat-bot-pana-light-mode.png";
-import minhaImagemEscura from "../../assets/Chat-bot-pana-dark-mode.png";
+import minhaImagem from "../../assets/image-light-mode.png";
+import minhaImagemEscura from "../../assets/image-dark-mode.png";
 
 export default function ({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
@@ -40,30 +40,32 @@ export default function ({ navigation }) {
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text
           style={{
-            fontSize: 30,
+            fontFamily: "Lato",
+            fontSize: 40,
             fontWeight: "bold",
             textAlign: "center",
             marginTop: -100,
             marginBottom: 50,
+            color: isDarkmode ? '#D9D9D9' : '#464444'
           }}
         >
           Bem vinde ao Departamento de Computação da UFSCar!
-        </Text>
-
-        <Image source={isDarkmode ? minhaImagemEscura : minhaImagem} style={{ width: 600, height: 600}} />
+        </Text> 
+        <Image 
+          source={isDarkmode ? minhaImagemEscura : minhaImagem} style={{ width: 950, height: 420}} />
 
         <Section style={{ position: "absolute", bottom: 10 }}>
           <SectionContent>
-            <Text fontWeight="bold" style={{ textAlign: "center" }}>
-            </Text>
+
             <Button
-              text="Toque aqui para iniciar"
+              style = {styles.buttonLight}
+              color = {isDarkmode ? '#9C86B8' : '#A0BDC6'}
+            
+              text = "Toque aqui para iniciar"
               onPress={() => {
-                navigation.navigate("Menu");
+                navigation.navigate("SecondScreen");
               }}
-              style={{
-                marginTop: 10,
-              }}
+              textStyle = {{color: isDarkmode ? '#D9D9D9' : '#464444'}}
             />
           </SectionContent>
         </Section>
@@ -71,3 +73,17 @@ export default function ({ navigation }) {
     </Layout>
   );
 }
+
+const styles = StyleSheet.create({
+  titulo: {
+    fontFamily: "Lato",
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 50,
+  },
+  button:{
+    marginTop: 10
+  },
+});
